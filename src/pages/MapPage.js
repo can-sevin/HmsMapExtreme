435ddd947b2ec0b2f11d2220469772e34aa1b911
Mahmut Can Sevin <mcancaglak@hotmail.com>
2020-10-19 16:36:53 +0300

    Ready for first publish 🥳

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,8 +1,11 @@
 import React, {Component, useState, useEffect} from 'react';
-import {ActivityIndicator, View} from 'react-native';
+import {ActivityIndicator, SafeAreaView, View, Text} from 'react-native';
 import RNHMSSite from '@hmscore/react-native-hms-site';
 import HMSLocation from '@hmscore/react-native-hms-location';
-import MapView, {Marker} from '@hmscore/react-native-hms-map';
+import MapView, {Marker, InfoWindow} from '@hmscore/react-native-hms-map';
+import {SearchBar, ListItem} from 'react-native-elements';
+
+let mapView, nearbySearchReq;
 
 const config = {
   apiKey:
@@ -13,6 +16,30 @@ const GetPermssions = () => {
   const [hasLocationPermission, setHasLocationPermission] = useState(false);
   const [position, setPosition] = useState();
   const [site, setSite] = useState([]);
+  const [search, setSearch] = useState(null);
+  const [searchList, setSearchList] = useState([]);
+  let updateSearch;
+
+  updateSearch = (Getsearch) => {
+    setSearch(Getsearch);
+    RNHMSSite.querySuggestion(nearbySearchReq)
+      .then((res) => {
+        setSite(res.sites);
+        setSearchList(res);
+
+        mapView.setCameraPosition({
+          target: {
+            latitude: res.sites[0].location.lat,
+            longitude: res.sites[0].location.lng,
+          },
+          zoom: 17,
+        });
+      })
+      .catch((err) => {
+        console.log(JSON.stringify(err) + 'Suggestion Error');
+      });
+  };
+
   useEffect(() => {
     HMSLocation.FusedLocation.Native.hasPermission()
       .then((result) => setHasLocationPermission(result))
@@ -20,17 +47,13 @@ const GetPermssions = () => {
   }, []);
   if (hasLocationPermission) {
     HMSLocation.FusedLocation.Native.getLastLocation()
-      .then((pos) => setPosition(pos))
+      .then((pos) => (position ? null : setPosition(pos)))
       .catch((err) => console.log('Failed to get last location', err));
     position
       ? RNHMSSite.initializeService(config)
           .then(() => {
-            console.log(
-              'Service is initialized successfully' +
-                position.longitude +
-                position.latitude,
-            );
-            let nearbySearchReq = {
+            nearbySearchReq = {
+              query: search,
               location: {
                 lat: position.latitude,
                 lng: position.longitude,
@@ -40,18 +63,25 @@ const GetPermssions = () => {
               countryCode: 'TR',
               language: 'tr',
               pageIndex: 1,
-              pageSize: 4,
+              pageSize: 8,
               politicalView: 'tr',
             };
-            RNHMSSite.nearbySearch(nearbySearchReq)
-              .then((res) => {
-                {
-                  setSite(res);
-                }
-              })
-              .catch((err) => {
-                console.log(JSON.stringify(err));
-              });
+            site.length === 0
+              ? RNHMSSite.nearbySearch(nearbySearchReq)
+                  .then((res) => {
+                    setSite(res.sites);
+                    mapView.setCameraPosition({
+                      target: {
+                        latitude: site[0].location.lat,
+                        longitude: site[0].location.lng,
+                      },
+                      zoom: 17,
+                    });
+                  })
+                  .catch((err) => {
+                    console.log(JSON.stringify(err));
+                  })
+              : null;
           })
           .catch((err) => {
             console.log('Error : ' + err);
@@ -62,7 +92,33 @@ const GetPermssions = () => {
   }
 
   return (
-    <View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <SearchBar
+        placeholder="Type Here..."
+        platform="default"
+        onChangeText={updateSearch}
+        value={search}
+        searchIcon={false}
+        lightTheme={true}
+      />
+      {search
+        ? Object.keys(searchList).map(function (l, i) {
+            return (
+              <ListItem key={i} bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>{searchList.sites[i].name}</ListItem.Title>
+                  <ListItem.Subtitle>
+                    {searchList.sites[i].formatAddress}
+                  </ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            );
+          })
+        : null}
+
       {position ? (
         <MapView
           camera={{
@@ -70,37 +126,94 @@ const GetPermssions = () => {
               latitude: position.latitude,
               longitude: position.longitude,
             },
-            zoom: 16,
+            zoom: 15,
           }}
+          ref={(e) => (mapView = e)}
           myLocationEnabled={true}
+          markerClustering={true}
           myLocationButtonEnabled={true}
           rotateGesturesEnabled={true}
           scrollGesturesEnabled={true}
           tiltGesturesEnabled={true}
           zoomGesturesEnabled={true}>
-          {site.sites != null
-            ? Object.keys(site.sites).map(function (key, i) {
+          {site != null
+            ? Object.keys(site).map(function (key, i) {
                 return (
-                  <Marker // Simple example
+                  <Marker
+                    visible={true}
                     coordinate={{
-                      latitude: site.sites[i].location.lat,
-                      longitude: site.sites[i].location.lng,
+                      latitude: site[i].location.lat,
+                      longitude: site[i].location.lng,
                     }}
-                    title={site.sites[i].name}
-                  />
+                    key={i}
+                    icon={{
+                      asset: 'huawei.png',
+                    }}
+                    clusterable>
+                    <InfoWindow
+                      style={{
+                        alignContent: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 8,
+                      }}>
+                      <View style={style.markerSelectedHms}>
+                        <Text
+                          style={style.titleSelected}>{`${site[i].name}`}</Text>
+                      </View>
+                    </InfoWindow>
+                  </Marker>
                 );
               })
-            : console.log('Site boş geliyor')}
+            : null}
         </MapView>
       ) : (
         <ActivityIndicator size="large" color="#0000ff" />
       )}
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default class MapPage extends Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = () => {
+    /*
+    let params = {
+      searchIntent: {
+        apiKey:
+          'CgB6e3x9edr9aDKC2hvAgHFWVKbcObkcj/nVGD/sjfcb4ZKV36LHmc5wS+WQqd782FtGwH+Cs2+GQsvSQTJONHPh',
+        hint: 'myhint',
+      },
+      searchFilter: {
+        query: 'Leeds',
+        language: 'en',
+      },
+    };
+    RNHMSSite.createWidget(params)
+      .then((res) => {
+        console.log(JSON.stringify(res) + 'Search Widget');
+      })
+      .catch((err) => {
+        console.log(JSON.stringify(err) + 'Search Widget');
+      }); */
+  };
+
   render() {
     return <GetPermssions />;
   }
 }
+
+const style = (base) => ({
+  markerSelectedHms: {
+    flexDirection: 'row',
+    height: 50,
+    borderRadius: base.radius.default,
+    overflow: 'hidden',
+    alignSelf: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+  },
+});
