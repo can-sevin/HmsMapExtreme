3fc4731122b1656ff85a3b2144f5a48c6bb82268
Mahmut Can Sevin <mcancaglak@hotmail.com>
2021-01-08 17:24:27 +0300

    v2

---
+++ src/markerImg.js
@@ -0,0 +1,12 @@
+export default {
+  /**
+   *  LOCATION ICONS
+   */
+  Ümraniye: 'metro/m5.png',
+  Üsküdar: 'metro/m5.png',
+  Kadıköy: 'metro/m4.png',
+  Maltepe: 'metro/m4.png',
+  Kartal: 'metro/m4.png',
+  Pendik: 'metro/m4.png',
+  Ataşehir: 'metro/m4.png',
+};
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -2,8 +2,17 @@ import React, {Component, useState, useEffect} from 'react';
 import {ActivityIndicator, SafeAreaView, View, Text} from 'react-native';
 import RNHMSSite from '@hmscore/react-native-hms-site';
 import HMSLocation from '@hmscore/react-native-hms-location';
-import MapView, {Marker, InfoWindow} from '@hmscore/react-native-hms-map';
+import MapView, {
+  Marker,
+  InfoWindow,
+  TileOverlay,
+  Polyline,
+  PatternItemTypes,
+  JointTypes,
+  CapTypes,
+} from '@hmscore/react-native-hms-map';
 import {SearchBar, ListItem} from 'react-native-elements';
+import markerImg from '../markerImg';
 
 let mapView, nearbySearchReq;
 
@@ -18,15 +27,17 @@ const GetPermssions = () => {
   const [site, setSite] = useState([]);
   const [search, setSearch] = useState(null);
   const [searchList, setSearchList] = useState([]);
+  const [points, setPoints] = useState([]);
+  let mPolylines = [];
+  let mPoints = [];
   let updateSearch;
 
   updateSearch = (Getsearch) => {
     setSearch(Getsearch);
     RNHMSSite.querySuggestion(nearbySearchReq)
       .then((res) => {
-        setSite(res.sites);
         setSearchList(res);
-
+        setSite(res.sites);
         mapView.setCameraPosition({
           target: {
             latitude: res.sites[0].location.lat,
@@ -40,11 +51,58 @@ const GetPermssions = () => {
       });
   };
 
+  let directionsApi = (mLat, mLon) => {
+    let requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        key:
+          'CgB6e3x9edr9aDKC2hvAgHFWVKbcObkcj/nVGD/sjfcb4ZKV36LHmc5wS+WQqd782FtGwH+Cs2+GQsvSQTJONHPh',
+      },
+      body: JSON.stringify({
+        origin: {
+          lng: position.longitude,
+          lat: position.latitude,
+        },
+        destination: {
+          lng: mLon,
+          lat: mLat,
+        },
+      }),
+    };
+
+    fetch(
+      'https://mapapi.cloud.huawei.com/mapApi/v1/routeService/walking?key=',
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data steps', data.routes[0].paths[0].steps);
+        let steep = data.routes[0].paths[0].steps;
+        steep.map((steps) => {
+          mPolylines.push(steps.polyline);
+        });
+        console.log('mPolylines', mPolylines);
+        if (mPolylines != null) {
+          mPolylines.map((point) => {
+            point.map((arr) => {
+              mPoints.push({latitude: arr.lat, longitude: arr.lng});
+            });
+          });
+          if (mPoints != null) {
+            setPoints(mPoints);
+            console.log('mPoints', mPoints);
+          }
+        }
+      });
+  };
+
   useEffect(() => {
     HMSLocation.FusedLocation.Native.hasPermission()
       .then((result) => setHasLocationPermission(result))
       .catch(HMSLocation.FusedLocation.Native.requestPermission());
   }, []);
+
   if (hasLocationPermission) {
     HMSLocation.FusedLocation.Native.getLastLocation()
       .then((pos) => (position ? null : setPosition(pos)))
@@ -58,18 +116,19 @@ const GetPermssions = () => {
                 lat: position.latitude,
                 lng: position.longitude,
               },
-              radius: 1000,
-              poiType: RNHMSSite.LocationType.BUS_STATION,
+              radius: 5000,
+              poiType: RNHMSSite.LocationType.SUBWAY_STATION,
               countryCode: 'TR',
               language: 'tr',
               pageIndex: 1,
-              pageSize: 8,
+              pageSize: 20,
               politicalView: 'tr',
             };
             site.length === 0
               ? RNHMSSite.nearbySearch(nearbySearchReq)
                   .then((res) => {
                     setSite(res.sites);
+                    console.log(JSON.stringify(res));
                     mapView.setCameraPosition({
                       target: {
                         latitude: site[0].location.lat,
@@ -145,10 +204,14 @@ const GetPermssions = () => {
                       latitude: site[i].location.lat,
                       longitude: site[i].location.lng,
                     }}
-                    key={i}
                     icon={{
-                      asset: 'huawei.png',
+                      asset: markerImg[site[i].address.subAdminArea],
                     }}
+                    key={i}
+                    zIndex={1}
+                    onClick={() =>
+                      directionsApi(site[i].location.lat, site[i].location.lng)
+                    }
                     clusterable>
                     <InfoWindow
                       style={{
@@ -165,6 +228,25 @@ const GetPermssions = () => {
                 );
               })
             : null}
+          {points != null ? (
+            <Polyline
+              points={points}
+              clickable={false}
+              geodesic={true}
+              color={-36351}
+              jointType={JointTypes.BEVEL}
+              pattern={[{type: PatternItemTypes.DASH, length: 20}]}
+              startCap={{
+                type: CapTypes.ROUND,
+              }}
+              endCap={{
+                type: CapTypes.ROUND,
+              }}
+              visible={true}
+              width={6.0}
+              zIndex={2}
+            />
+          ) : null}
         </MapView>
       ) : (
         <ActivityIndicator size="large" color="#0000ff" />
